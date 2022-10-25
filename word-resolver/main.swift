919ff2aa63658b05888b228d09740df0d6b9dc9a
Igor Kislyuk <i.kislyuk@tinkoff.ru>
2022-10-25 11:45:43 +0300

    Initial commit

---
+++ word-resolver/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  word-resolver
+//
+//  Created by Igor Kislyuk on 25.10.2022.
+//
+
+import Foundation
+
+print("Welcome to work resolver!")
+while let input = readLine() {
+    if input == "*****" {
+        break
+    }
+
+    print("Your input is \(input)")
+}
